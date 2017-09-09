36c4affcb3cfeed92d82200f18f697c0cdb85310
Diego Mendoza <dmendoza@inflexionsoftware.com>
2017-09-08 22:16:21 -0500

    Agregue la funcionalidad para que al seleccionar una foto de la galería se muestre a tamaño completo

--- js/app/home/home.js
+++ js/app/home/home.js
@@ -3,17 +3,66 @@
 
 	angular.module('chaiApp.home').controller('HomeCtrl', HomeCtrl);
 
-	HomeCtrl.$inject = ['$routeParams'];
+	
+
+	/*
+	*Modal controller
+	*/
+	angular.module('chaiApp.home').controller('ModalCtrl', ModalCtrl);
+
+	ModalCtrl.$inject = ['$scope', '$uibModalInstance', 'item'];
+
+	function ModalCtrl($scope, $uibModalInstance, item){
+		var modalCtrl = this;
+
+		//methods
+		modalCtrl.close = close;
+
+		//Properties
+		modalCtrl.image =item.image;
+
+		activate();
+
+		function activate(){
+			//Using control parameter 
+			console.log('Modal is active');
+		}
+
+		function close(){
+			$uibModalInstance.close( true );
+		}
+	}
+
+	HomeCtrl.$inject = ['$routeParams', '$uibModal'];
 
 	/**@ngInject*/
-	function HomeCtrl($routeParams){
+	function HomeCtrl($routeParams, $uibModal){
 		var homeCtrl = this;
+
+		//Events
+		homeCtrl.showModal = showModal;
 
 		//Initialize controller
 		activate();
 
 		function activate(){
 			console.log('Activated HomeCtrl');	
+		}
+
+		function showModal(srcImage){
+			var item = { image: srcImage };
+			var modalInstance = $uibModal.open({
+		        templateUrl: 'js/app/home/photo-modal.html',
+		        controller: 'ModalCtrl',
+		        controllerAs: 'modalCtrl',
+		        size: 'lg',
+		        resolve: {
+		        	//selected item
+					item: function () {
+						return item;
+					}
+				}
+		    });
 		}
 	}
 })();
--- js/app/home/home.module.js
+++ js/app/home/home.module.js
@@ -1,4 +1,4 @@
 ( function() {
 	'use strict';
-	angular.module('chaiApp.home', []);
+	angular.module('chaiApp.home', ['ui.bootstrap']);
 })();
